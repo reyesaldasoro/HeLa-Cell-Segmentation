cdf2d4e89869064d8ad6c6ea53a6be897b3c6f6a
CCRA <reyesaldasoro@googlemail.com>
2018-05-22 12:07:21 +0100

    Use previous segmentation
    Quite a few small changes in the code, but the main one is the use of a previousSegmentation, i.e. use segmentation of slice n to guide the segmentation of slice n+1 or n-1, in this way it is possible to detect disjoint regions.

--- segmentNucleiHelaEM.m
+++ segmentNucleiHelaEM.m
@@ -1,9 +1,10 @@
-function nucleiHela = segmentNucleiHelaEM(Hela,cannyStdValue))
-%function nucleiHela = segmentNucleiHelaEM(Hela,cannyStdValue))
+function nucleiHela = segmentNucleiHelaEM(Hela,cannyStdValue,previousSegmentation)
+%function nucleiHela = segmentNucleiHelaEM(Hela,cannyStdValue,previousSegmentation)
 %--------------------------------------------------------------------------
-% Input         Hela       : an image in Matlab format,it can be 2D/3D, double/uint8
-%               cannyStdValue: the value of the Std of the canny edge detection
-% Output        nucleiHela : a binary image with 1 for nuclei, 0 background
+% Input         Hela                    : an image in Matlab format,it can be 2D/3D, double/uint8
+%               cannyStdValue           : the value of the Std of the canny edge detection
+%               previousSegmentation    : segmentation of slice above/below
+% Output        nucleiHela              : a binary image with 1 for nuclei, 0 background
 %--------------------------------------------------------------------------
 % 
 % This code segments the nuclei of HeLa Cells that have been acquired with Electron
@@ -41,36 +42,38 @@ function nucleiHela = segmentNucleiHelaEM(Hela,cannyStdValue))
 
 
 % Check dimensions of the data
-[rows,cols,levs]=size(Hela);
+[rows,cols,levs]            = size(Hela);
 
 % Restrict to one channel and change to double if necessary
 if levs>1
-    Hela =double(Hela(:,:,1));
+    Hela                    = double(Hela(:,:,1));
 end
 if ~isa(Hela,'double')
-    Hela =double(Hela);
+    Hela                    = double(Hela);
 end
-if ~exist('cannyStdValue','var')
+% Check the existance of Canny value,
+if (~exist('cannyStdValue','var'))
+    cannyStdValue            = 4;
+end
+if (isempty(cannyStdValue))
     cannyStdValue            = 4;
 end
 
 % Low pass filter for future operations
-Hela_LPF = imfilter(Hela,fspecial('Gaussian',7,2));
+Hela_LPF                    = imfilter(Hela,fspecial('Gaussian',7,2));
 %% Find edges and distance from those edges
-Hela_Edge           = edge(Hela,'canny',[],cannyStdValue);
-
-
+Hela_Edge                   = edge(Hela,'canny',[],cannyStdValue);
 
 %% processing of the edges could be done
-Hela_Edge_L     = bwlabel(Hela_Edge);
-Hela_Edge_LR    = regionprops(Hela_Edge,Hela_LPF,'area','eccentricity','meanintensity');
-Hela_Edge_L2     = bwlabel(ismember(Hela_Edge_L,find(( ([Hela_Edge_LR.Eccentricity]<145)&([Hela_Edge_LR.Area]>4) ) )));
+% Hela_Edge_L               = bwlabel(Hela_Edge);
+% Hela_Edge_LR              = regionprops(Hela_Edge,Hela_LPF,'area','eccentricity','meanintensity');
+% Hela_Edge_L2              = bwlabel(ismember(Hela_Edge_L,find(( ([Hela_Edge_LR.Eccentricity]<145)&([Hela_Edge_LR.Area]>4) ) )));
 
 
 %% Find location to keep the region at the centre and discard those that are close
 % to the edges of the image
 
-Hela_Edge_D     = bwdist(Hela_Edge);
+Hela_Edge_D                 = bwdist(Hela_Edge);
 
 % THE DISTANCE DETERMINES WHEN EDGES WILL BE CONNECTED, WITH LARGER CANNY VALUES, THE
 % EDGES MAY NOT CONNECT AT 5, SO BETTER TO INCREASE WITH THE CANNY VALUE
@@ -85,47 +88,84 @@ Hela_Centroid2      = bwlabel(Hela_Centroid==0);
 Hela_Centroid3      = regionprops(Hela_Centroid2,'Area','Centroid','eccentricity','extrema');
 
 [Hela_Centroid4,numR]      = bwlabel(ismember(Hela_Centroid2,find([Hela_Centroid3.Area]>10000)));
-Hela_Centroid5      = regionprops(Hela_Centroid4,'Area','Centroid','eccentricity','extrema');
 
-% Detect the extrema of regions
-Hela_ExtremaMin = min(reshape(min([Hela_Centroid5.Extrema]),[2,numR]));
-Hela_ExtremaMax = max(reshape(max([Hela_Centroid5.Extrema]),[2,numR]));
-% detect the extrema that will touch the first / last columns and rows, notice that
-% it is expected that rows and columns are equal, but this could have an impact with
-% rectangular images
+if ~exist('previousSegmentation','var')
+    
+    Hela_Centroid5      = regionprops(Hela_Centroid4,'Area','Centroid','eccentricity','extrema');
+    
+    % Detect the extrema of regions
+    Hela_ExtremaMin     = min(reshape(min([Hela_Centroid5.Extrema]),[2,numR]));
+    Hela_ExtremaMax     = max(reshape(max([Hela_Centroid5.Extrema]),[2,numR]));
+    % detect the extrema that will touch the first / last columns and rows, notice that
+    % it is expected that rows and columns are equal, but this could have an impact with
+    % rectangular images
+    
+    testExtreme1        = Hela_ExtremaMin<1;
+    testExtreme2        = Hela_ExtremaMax>(min(rows,cols));
+    
+    % discard regions in the edges and at the same time fill holes in those that are kept
+    % NOTE: WITH VERY LARGE REGIONS, THIS MAY BE THE REGION IN THE CENTRE WHICH EXPANDS
+    % TO THE EDGES, THIS IS ESPECIALLY THE CASE WITH LARGE VALUES OF CANNY.
+    
+    Nuclei_0            = imfill(ismember(Hela_Centroid4,find(~(testExtreme1|testExtreme2))),'holes');
+    
+    % repeat labelling process to detect small and bringther regions and keep the region in the centre
+    [Nuclei_1,numN]     = bwlabel(Nuclei_0);
+    %Nuclei_2            = regionprops(Nuclei_1,Hela,'Area','MeanIntensity','Centroid');
+    % % Centre the centroid with respect to rows and columns
+    % centralRegionCoord=sqrt(sum((reshape([Nuclei_2.Centroid],[2 numN])'-repmat([cols/2 rows/2],numN,1)).^2,2));
+    % % select the region which is closest to rows/2, cols/2
+    % [minCentroid,posCentroid]= min(centralRegionCoord);
+    % Nuclei_3  = (Nuclei_1==posCentroid);
+    if numN ==0
+        nucleiHela = zeros(rows,cols);
+        return
+    end       
+    
+    % Selection of centre-most region
+    % The selection of the centremost region can be done with centroids, but if the
+    % centre-most is slighlty shifted to the side and there is an annular shape that goes
+    % around the central region, its centroid can be more in the centre and get
+    % incorrectly selected. It is better to create a distance map from centroid, and use
+    % that as an intensity map, then the region with lowest intensity would be selected
+    % as centre-most
+    
+    Nuclei_DistFromCentre                               = zeros(rows,cols);
+    Nuclei_DistFromCentre(round(rows/2),round(cols/2))  = 1;
+    Nuclei_DistFromCentre                               = bwdist(Nuclei_DistFromCentre);
+    
+    Nuclei_2B = regionprops(Nuclei_1,Nuclei_DistFromCentre,'Area','MinIntensity','MeanIntensity','MaxIntensity','Centroid');
 
-testExtreme1 = Hela_ExtremaMin<1;
-testExtreme2 = Hela_ExtremaMax>(min(rows,cols));
-
-% discard regions in the edges and at the same time fill holes in those that are kept
-% NOTE: WITH VERY LARGE REGIONS, THIS MAY BE THE REGION IN THE CENTRE WHICH EXPANDS
-% TO THE EDGES, THIS IS ESPECIALLY THE CASE WITH LARGE VALUES OF CANNY.
-
-Nuclei_0 = imfill(ismember(Hela_Centroid4,find(~(testExtreme1|testExtreme2))),'holes');
-
-%% repeat labelling process to detect small and bringther regions and keep the region in the centre 
-[Nuclei_1,numN] = bwlabel(Nuclei_0);
-Nuclei_2 = regionprops(Nuclei_1,Hela,'Area','MeanIntensity','Centroid');
-
-if numN ==0
-    nucleiHela = zeros(rows,cols);
-    return
+    % Centre the centroid with respect to rows and columns
+    %centralRegionCoord=sqrt(sum((reshape([Nuclei_2.Centroid],[2 numN])'-repmat([cols/2 rows/2],numN,1)).^2,2));
+    % select the region which is closest to rows/2, cols/2
+    %[minCentroid,posCentroid]= min(centralRegionCoord);
+    
+    % select the region which is closest to rows/2, cols/2
+    [minCentroid,posCentroid]   = min([Nuclei_2B.MinIntensity]);    
+    Nuclei_3                    = (Nuclei_1==posCentroid);
+else
+    % If a previous segmentation exists, keep only those regions that are in contact
+    % with the segmentation, as the segmentations are going up and down, it is
+    % necessary to erode, however, if the erosion is too much, it will remove
+    % everything
+    
+    Nuclei_0                    = Hela_Centroid4.*imerode(previousSegmentation,ones(20));
+    Nuclei_0P                   = regionprops(Nuclei_0,'area');
+    %regionsToKeep               = unique(Nuclei_0);
+    regionsToKeep               = find([Nuclei_0P.Area]>100);
+    Nuclei_1                    = ismember(Hela_Centroid4, regionsToKeep);
+    Nuclei_3                    = imfill((Nuclei_1),'holes');
 end
-% Centre the centroid with respect to rows and columns
-centralRegionCoord=sqrt(sum((reshape([Nuclei_2.Centroid],[2 numN])'-repmat([cols/2 rows/2],numN,1)).^2,2));
-% select the region which is closest to rows/2, cols/2
-[minCentroid,posCentroid]= min(centralRegionCoord);
-Nuclei_3  = (Nuclei_1==posCentroid);
-
 
 %% Obtain a distance transform to calculate the intensities of increasingly distant lines
 
-Nuclei_4 = round(bwdist(Nuclei_3));
+Nuclei_4                        = round(bwdist(Nuclei_3));
 
 %% find how the intensity changes from the edge of the nuclei so far
-Nuclei_6(50)=0;
+Nuclei_6(50)                    = 0;
 for k=1:50
-    Nuclei_6(k) =median(Hela_LPF(Nuclei_4==k));
+    Nuclei_6(k)                 =median(Hela_LPF(Nuclei_4==k));
 end
 
 % this should present a line that decreases up to a groove, corresponding to the
@@ -136,13 +176,14 @@ end
 % pixel is below a certain intensity level (determined by previous step) it is
 % discarded. Once the number of valid pixels is below a threshold (150, it starts on
 % several thousands) the process stops. Small boundary regions are discarded to
-% prevent the region growing to flow outside the nuclei.
+% prevent the region growing to flow outside the nuclei through a small gap. Also the
+% number of dilations are restricted to 25 for the same reason
 
 initRegion  = Nuclei_3;
 k=0;
 numPixBoundary = inf;
 
-while numPixBoundary>150
+while (numPixBoundary>150)&(k<25)
     k=k+1;
     disp(k)
     dilRegion   = imdilate(initRegion>0,ones(3))-initRegion;
@@ -161,26 +202,9 @@ nucleiHela = imfill(nucleiHela,'holes');
 %% Final dilation
 % Initial experiments showed that the result slightly underestimate the nuclei, so
 % dilate by 5
-nucleiHela = imdilate(nucleiHela,ones(5));
+%nucleiHela = imdilate(nucleiHela,ones(5));
+nucleiHela = imdilate(nucleiHela,strel('disk',5));
 
-
-% %% Extend to detect background automatically
-% 
-% % Use a much blurrier edge detection to create large superpixels to detect the
-% % background
-% 
-% Hela_Edge       = imdilate(edge(Hela,'canny',[],9),ones(9));
-% Hela_supPix     = bwlabel(1-Hela_Edge);
-% Hela_supPixR    = regionprops(Hela_supPix,Hela_LPF,'Area','meanintensity');
-% Hela_supPixBrightLarge = ismember(Hela_supPix,find(([Hela_supPixR.MeanIntensity]>180)&( [Hela_supPixR.Area]>10000 )   )  );
-% Hela_background = imfill(Hela_supPixBrightLarge,'holes');
-% 
-
-
-%%
-% finalResults (:,:,1) = Hela_LPF;
-% finalResults (:,:,2) = Hela_LPF+100*nucleiHela;
-% finalResults (:,:,3) = Hela_LPF+ 100*Hela_background;
 
 
 
