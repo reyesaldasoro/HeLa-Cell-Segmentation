16e2e95d206931f91fe5560e269f5d5fa4628492
Constantino Carlos Reyes-Aldasoro <reyesaldasoro@googlemail.com>
2018-07-02 11:56:23 +0100

    Output Folder named after XYZ coordinates

--- segmentHelaROI.m
+++ segmentHelaROI.m
@@ -1,4 +1,4 @@
-function segmentHelaROI(inputFolder,outputFolder,IndividualHelaLabels,numberOfLabel,inputSlice)
+function segmentHelaROI(inputFolder,IndividualHelaLabels,numberOfLabel,inputSlice)
 % function segmentHelaROI(inputFolder,outputFolder,IndividualHelaLabels,numberOfLabel,inputSlice)
 %--------------------------------------------------------------------------
 % Input         inputFolder         : location of the input stack
@@ -73,6 +73,71 @@ if numFilesIn==0
     return
 end
 
+%% The output Folder will be created with the name ROI_X_Y_Z coordinates
+
+%% The region is crucial for detection and naming of the output folder
+% The mask for the cell is required
+if ~exist('IndividualHelaLabels','var')
+    help segmentHelaROI;
+    return
+end
+
+if ~exist('numberOfLabel','var')
+    numberOfLabel              =  1;
+end
+
+[rows,cols,levs]               =  size(IndividualHelaLabels);
+if levs>1
+    CurrentHelaLabel            =  IndividualHelaLabels(:,:,numberOfLabel);
+end
+% Detect centroid of Labelled region
+posLabel                       =  regionprops(CurrentHelaLabel,'Area','BoundingBox','Centroid');
+
+%% select ROI
+
+
+% Find the coordinates that will span 2000 x 2000 centred at the centroid, be careful
+% with regions that are near the boundaries, for the time being, shift the box, but
+% that would make the cell not be centred.
+
+rInit                          =  ((posLabel.Centroid(2) - 1000));
+cInit                          =  ((posLabel.Centroid(1) - 1000));
+
+if (rInit<1)||(rInit>rows)
+    % For the time being shift the ROI, could also pad with zeros or discard
+    % altogether
+    rInit                      =  min(max(1,posLabel.Centroid(2) - 1000),rows-2000);
+end
+if (cInit<1)||(cInit>cols)
+    % For the time being shift the ROI, could also pad with zeros or discard
+    % altogether
+    cInit                      =  min(max(1,posLabel.Centroid(1) - 1000),cols-2000);
+end
+
+% Once the initial positions have been calculated, find the final positions
+rFin                           =  rInit+1999;
+cFin                           =  cInit+1999;
+
+%%
+
+
+%% Read all the files that need to be extracted, crop and save
+
+% The input slice is useful, however if not provided, the central slice is selected
+if ~exist('inputSlice','var')
+    inputSlice                 =  max(1,floor(numFilesIn/2));
+end
+
+zInit                          =  max(1,inputSlice-300);
+zFin                           =  min(numFilesIn,inputSlice+300);
+
+%% Output folder name
+outputLocation                  = strcat('ROI_',num2str(rInit),'-',num2str(cInit),'-',num2str(zInit));
+outputFolder                    = strcat(outputLocation,'_',num2str(numberOfLabel));
+
+
+
+
 % if output does not exist, it will be created.
 if ~exist(outputFolder,'dir')
     disp('----------------------------');
@@ -99,66 +164,19 @@ end
 
 %% Input and Output Folders exist. Check other parameters
 
-% The mask for the cell is required
-if ~exist('IndividualHelaLabels','var')
-    help segmentHelaROI;
-    return
-end
 
-if ~exist('numberOfLabel','var')
-    numberOfLabel              =  1;
-end
-
-[rows,cols,levs]               =  size(IndividualHelaLabels);
-if levs>1
-    IndividualHelaLabels       =  IndividualHelaLabels(:,:,numberOfLabel);
-end
-
-% The input slice is useful, however if not provided, the central slice is selected
-if ~exist('inputSlice','var')
-    inputSlice                 =  max(1,floor(numFilesIn/2));
-end
-
-%% All parameters are ready, select ROI
-
-% Detect centroid of Labelled region
-posLabel                       =  regionprops(IndividualHelaLabels,'Area','BoundingBox','Centroid');
-
-% Find the coordinates that will span 2000 x 2000 centred at the centroid, be careful
-% with regions that are near the boundaries, for the time being, shift the box, but
-% that would make the cell not be centred.
-
-rInit                          =  ((posLabel.Centroid(2) - 1000));
-cInit                          =  ((posLabel.Centroid(1) - 1000));
-
-if (rInit<1)||(rInit>rows)
-    % For the time being shift the ROI, could also pad with zeros or discard
-    % altogether
-    rInit                      =  min(max(1,posLabel.Centroid(2) - 1000),rows-2000);
-end
-if (cInit<1)||(cInit>cols)
-    % For the time being shift the ROI, could also pad with zeros or discard
-    % altogether
-    cInit                      =  min(max(1,posLabel.Centroid(1) - 1000),cols-2000);
-end
-
-% Once the initial positions have been calculated, find the final positions
-rFin                           =  rInit+1999;
-cFin                           =  cInit+1999;
-
-%% Read all the files that need to be extracted, crop and save
-
-zInit                          =  max(1,inputSlice-300);
-zFin                           =  min(numFilesIn,inputSlice+300);
 
 %%
 for counterFiles=zInit:zFin
     % Decide on how to name the files
     inFileName                 =  strcat(inputFolder,'/',dirIn(counterFiles).name);
-    outFileName                =  strcat(outputFolder,'/ROI_',num2str(numberOfLabel),'_',dirIn(counterFiles).name);
+    findExtension              = strfind(inFileName,'.');
+    outFileName                =  strcat(outputLocation,'_z',inFileName(findExtension-4:end));
+    
+    %outFileName                =  strcat(outputFolder,'/ROI_',num2str(numberOfLabel),'_',dirIn(counterFiles).name);
     tempFile1                  =  imread(inFileName);
     tempFile                   =  tempFile1(rInit:rFin, cInit:cFin);
-    imwrite(tempFile,outFileName)
+    imwrite(tempFile,strcat(outputFolder,'/',outFileName))
 end
 
 
