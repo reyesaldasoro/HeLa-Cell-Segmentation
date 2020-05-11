c7f1abf5e828eddc5b9cb685cc4c64d610101408
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2020-05-11 17:49:31 +0100

    Update segmentHelaMitochondriaEM.m

--- Code/segmentHelaMitochondriaEM.m
+++ Code/segmentHelaMitochondriaEM.m
@@ -7,11 +7,14 @@ Hela_LPF                                    = imfilter(Hela,filtG,'replicate');
  Hela_LPF(imdilate(nucleiHela==1,ones(9)))                 = avNucleiIntensity;
  Hela_LPF(Hela_LPF>(Background_intensity-10))         = avNucleiIntensity;
  %%
- darkRegions                        = bwlabel( Hela_LPF<(avNucleiIntensity*0.3+0.7*envelopeIntensity));
+ darkRegions                        = bwlabel( Hela_LPF<(avNucleiIntensity*0.1+0.9*envelopeIntensity));
  darkRegions_R                      = regionprops(darkRegions,'Area','Eccentricity','solidity');
  %%
  [darkRegions_large,numL]           = bwlabel( ismember (darkRegions,find(([darkRegions_R.Area]>200).*([darkRegions_R.Eccentricity]<0.97)   )));
- %darkRegions_large_R    = regionprops(darkRegions_large,'Area','Eccentricity','solidity');
+ darkRegions_large_R    = regionprops(darkRegions_large,'Area','Eccentricity','solidity','MajoraxisLength','MinoraxisLength','Eccentricity');
+ 
+ 
+ %%
  temp                               = zeros(size(darkRegions));
  structEl = strel('disk',8);
  for counterR = 1:numL
@@ -20,19 +23,23 @@ Hela_LPF                                    = imfilter(Hela,filtG,'replicate');
    %%  
  darkRegions_large2                 = bwlabel( imopen(temp,strel('disk',3)));
 % darkRegions_large3     = 
- darkRegions_R2                     = regionprops(darkRegions_large2,'Area','Eccentricity','solidity');
+ darkRegions_R2                     = regionprops(darkRegions_large2,'Area','Eccentricity','solidity','MajoraxisLength','MinoraxisLength','Eccentricity');
  [darkRegions_large3,numL]          = bwlabel( ismember (darkRegions_large2,find(([darkRegions_R2.Area]>800))   ));
  %%
 
  
- filtG                                       = gaussF(3,3,1);
-Hela_LPF                                    = imfilter(Hela,filtG,'replicate');
- Mitochondria_0                     = imfill(darkRegions_large3,'holes');
- Mitochondria_1                     = edge((Mitochondria_0>0).*Hela,'canny',[],2.5);
+filtG                                       = gaussF(3,3,1);
+%Hela_LPF                                    = imfilter(Hela,filtG,'replicate');
+
+Hela_Edges                          = edge(Hela,'canny',[],2.5);
+
+Mitochondria_0                     = imfill(darkRegions_large3,'holes');
+Mitochondria_1                     = (edge((Mitochondria_0>0).*Hela,'canny',[],2.5));
 
 %imagesc(51*(darkRegions_large3>0)+Hela_LPF)
  %imagesc((Mitochondria_0>0).*Hela_LPF)
- imagesc(Mitochondria_1)
+ imagesc(Mitochondria_1+Mitochondria_0)
  
+ % Candidate for Mito
  
  mitochondria                       = darkRegions_large3;
