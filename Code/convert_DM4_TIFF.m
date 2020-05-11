3c06b927d4623cc400c886e23ee845b3dae0cad2
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2020-05-11 17:49:09 +0100

    Create convert_DM4_TIFF.m
    Read files in DM4 format, downloaded from EMPIAR and convert to uint8 tiffs,

---
+++ Code/convert_DM4_TIFF.m
@@ -0,0 +1,29 @@
+%% Convert all the DM4 UINT32 images to TIFF  UINT8 images to keep consistency with Crick previous formats
+clear all
+close all
+
+baseDir             = 'D:\Acad\Crick\Hela8000\';
+outputDir           = 'D:\Acad\Crick\Hela8000_tiff\';
+
+dir0                = dir(strcat(baseDir,'*dm4'));
+
+numImages           = size(dir0,1);
+
+%%
+%filtG= gaussF(3,3,1);
+
+for k=1:numImages
+    disp(k)
+    [c,d]=dmread(dir0(k).name);
+
+    % To convert from uint32 to uint8 and keep the same range of values than the cropped ROIs
+    d3=double(d); 
+    d4=(d3-mean(d3))/std(d3(:));
+    d5=165+35*d4;
+    d5(d5<0)=0;d5(d5>256)=256;    
+    d6=uint8(d5);
+    imwrite(d6,strcat(outputDir,'HeLa_8000_',dir0(k).name(end-7:end-4),'.tiff'));
+end
+    
+
+%%
