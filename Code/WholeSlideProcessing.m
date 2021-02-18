77f91bb5f8087b3ac647cf0f991aadc14c7cfd86
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-02-18 19:37:27 +0000

    Create WholeSlideProcessing.m

---
+++ Code/WholeSlideProcessing.m
@@ -0,0 +1,7 @@
+baseDir                         = 'C:\Users\sbbk034\Documents\Acad\Crick\Hela8000_tiff\';
+tic;
+[final_coords,final_centroid,final_cells,final_dist]        = detectNumberOfCells_3D(baseDir,20,1);
+t1=toc;
+tic
+listFolders = generate_ROI_Hela (baseDir,final_coords,final_centroid);
+t2=toc;
