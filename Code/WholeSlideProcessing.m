c33717b0027aa5e8b2a117898ffc265323367be4
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-02-19 11:00:03 +0000

    Update WholeSlideProcessing.m

--- Code/WholeSlideProcessing.m
+++ Code/WholeSlideProcessing.m
@@ -2,6 +2,13 @@ baseDir                         = 'C:\Users\sbbk034\Documents\Acad\Crick\Hela800
 tic;
 [final_coords,final_centroid,final_cells,final_dist]        = detectNumberOfCells_3D(baseDir,20,1);
 t1=toc;
+%%
 tic
 listFolders = generate_ROI_Hela (baseDir,final_coords,final_centroid);
 t2=toc;
+%%
+tic
+Hela_background 	= segmentBackgroundHelaEM(Hela);
+Hela_nuclei     	= segmentNucleiHelaEM(Hela);    
+
+t3=toc;
