716f787c811da37bc6b1aa4f9502cb40ea616324
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-03-24 11:26:44 +0000

    Update Fig3.m

--- Figures/Fig3.m
+++ Figures/Fig3.m
@@ -1,113 +1,135 @@
-% Fig3
+% Fig3.m 
 clear all
 close all
-load('Results_ROI_1656_6756_329_2021_03_09B.mat')
+
+%% Define the base directory where the data (8000x8000 slices) is stored
+baseDir                         = 'C:\Users\sbbk034\Documents\Acad\Crick\Hela8000_tiff\';
+
+%% Detect the number of cells to be segmented, record centroids and coordinates
+% Currently all ROIs are forced to be 2,000 x 2,000 x 300 This may not
+% always be the best as the centroid may be far from the centre of the ROI,
+% if it is above/below slice 150 the segmentation may not be well
+% performed. 
+tic;
+[final_coords,final_centroid,final_cells,final_dist,positionROI2]        = detectNumberOfCells_3D(baseDir,20);
+t1=toc;
 
 %%
-% This is for the slices to create the surfaces 
-[rows,cols,levs]        = size(Hela_nuclei);
-numSlices               = levs;
-[x2d,y2d]               = meshgrid(1:rows,1:cols);
-z2d                     = ones(rows,cols);
-xx_3D                   = zeros(rows,cols,levs);
-yy_3D                   = zeros(rows,cols,levs);
-zz_3D                   = zeros(rows,cols,levs);
+gaussFilt       = fspecial('Gaussian',3,1);
 
-for k=1:numSlices
-    disp(k)
-    zz_3D(:,:,k)        = ones(rows,cols)*k;
-    xx_3D(:,:,k)        = x2d;
-    yy_3D(:,:,k)        = y2d;    
-end
+dir0                = dir(strcat(baseDir,filesep,'*.t*'));
+numFiles            = size(dir0,1);
+fstep               = 8;
 
+sliceToRead               = 1;
+Hela_3D         = ( imfilter(imread(strcat(baseDir,dir0((sliceToRead)).name)), gaussFilt));
+    [rows,cols,levs]        = size(Hela_3D);
+    [x2d,y2d]               = meshgrid(1:rows,1:cols);
+    z2d                     = ones(rows,cols);
+        fstep               = 8;
+    %%
+
+    figure
+    h121=subplot(121);
+hold on    
+        startX              = 1; %currentSlice*10;
+    xCoord              = x2d(startX:fstep:end,1:fstep:end);
+    yCoord              = y2d(startX:fstep:end,1:fstep:end);
+    zCoord              = z2d(startX:fstep:end,1:fstep:end);
+    imageCoord          = Hela_3D(startX:fstep:end,1:fstep:end);
+    hSurf = surf(xCoord,yCoord,sliceToRead*zCoord,imageCoord);
+    hSurf.EdgeColor='none';
+    axis tight
+    
+    
+    %
+    
+       hold on
+    numSlicesProb   = size(positionROI2,3);
+    numCells        = size(positionROI2,1);
+    for k=1:numSlicesProb
+        for kk=1:numCells
+            %        text(positionROI(kk,2,k),positionROI(kk,1,k),k,num2str(kk+(k-1)*numCells),'color',[k/numSlicesProb 0 (numSlicesProb-k)/numSlicesProb])
+            text(positionROI2(kk,2,k),positionROI2(kk,1,k),k,num2str(kk),'color',[k/numSlicesProb 0 (numSlicesProb-k)/numSlicesProb],'fontsize',8)
+        end
+    end
+    
+    axis([1 rows 1 cols 1 numSlicesProb])
+    grid on
+    axis ij
+    rotate3d on
+    colormap gray
+    %
+    numFinalCells = size(final_cells,1);
+    %currLine =[];
+    for k=1:numFinalCells
+        levsToPlot = find(final_cells(k,:));
+        cellsToPlot = final_cells(k,levsToPlot);
+        clear currLine
+        currLine =[];
+        for k2 =1:numel(levsToPlot)
+            currLine = [currLine;[positionROI2(cellsToPlot(k2),:,levsToPlot(k2)) levsToPlot(k2) ]];
+        end
+        plot3(currLine(:,2),currLine(:,1),currLine(:,3),'linewidth',2)
+    end
+        title('(a)','fontsize',18)
+   %%
+      h122=subplot(122);
+hold on    
+        startX              = 1; %currentSlice*10;
+    xCoord              = x2d(startX:fstep:end,1:fstep:end);
+    yCoord              = y2d(startX:fstep:end,1:fstep:end);
+    zCoord              = z2d(startX:fstep:end,1:fstep:end);
+    imageCoord          = Hela_3D(startX:fstep:end,1:fstep:end);
+    hSurf = surf(xCoord,yCoord,sliceToRead*zCoord,imageCoord);
+    hSurf.EdgeColor='none';
+    axis tight
+    
+
+
+
+    
+       hold on
+    numSlicesProb   = size(positionROI2,3);
+    numCells        = size(positionROI2,1);
+    for k=1:numSlicesProb
+        for kk=1:numCells
+            %        text(positionROI(kk,2,k),positionROI(kk,1,k),k,num2str(kk+(k-1)*numCells),'color',[k/numSlicesProb 0 (numSlicesProb-k)/numSlicesProb])
+            text(positionROI2(kk,2,k),positionROI2(kk,1,k),k,num2str(kk),'color',[k/numSlicesProb 0 (numSlicesProb-k)/numSlicesProb],'fontsize',8)
+        end
+    end
+    
+    axis([1 rows 1 cols 1 numSlicesProb])
+    grid on
+    axis ij
+    rotate3d on
+    colormap gray
+    %
+    numFinalCells = size(final_cells,1);
+    %currLine =[];
+    for k=1:numFinalCells
+        levsToPlot = find(final_cells(k,:));
+        cellsToPlot = final_cells(k,levsToPlot);
+        clear currLine
+        currLine =[];
+        for k2 =1:numel(levsToPlot)
+            currLine = [currLine;[positionROI2(cellsToPlot(k2),:,levsToPlot(k2)) levsToPlot(k2) ]];
+        end
+        plot3(currLine(:,2),currLine(:,1),currLine(:,3),'linewidth',2)
+    end 
+        title('(b)','fontsize',18)
+        
 %%
+h121.ZTickLabel=h121.ZTick*20;
+h122.ZTickLabel=h121.ZTick*20;
+set(gcf,'position',[ 500  200  900  450])
 
-%% This is for the surface 
-% We could create the surface directly with this, but as the volume is rather large,
-% the number of faces of the surface would be rather high, it would be slow and may
-% crash in a computer with low memory. This it is better to generate the reference
-% framework to create a isosurface with fewer faces
-
-% We can now generate the isosurface of the cell, with a certain step; using fstep =1
-% would be the same as the whole surface. With 8/16, the results are still visually good
-% and hard to distinguish with smaller steps.
-
-maxSlice            = levs;
-minSlice            = 1;
+h121.View = [-10 80];
+h122.View = [15 20];
 
 
-
-% Colours
-jet2    = jet;
-jet3    = jet2(round(linspace(1,256,numFiles)),:);
-[a,b]   = sort(rand(numFiles,1));
-[c,d]   = sort(rand(numFiles,1));
-
-%%
-fstep               = 4;
-figure
-        surf_Nuclei          = isosurface(yy_3D(1:fstep:end,1:fstep:end,minSlice:maxSlice)   ,...
-                                          xx_3D(1:fstep:end,1:fstep:end,minSlice:maxSlice)  ,...
-                                          zz_3D(1:fstep:end,1:fstep:end,minSlice:maxSlice),...
-                                    Hela_nuclei(1:fstep:end,1:fstep:end,minSlice:maxSlice),0.7);
-        surf_Cell          = isosurface(yy_3D(1:fstep:end,1:fstep:end,minSlice:maxSlice)   ,...
-                                          xx_3D(1:fstep:end,1:fstep:end,minSlice:maxSlice) ,...
-                                          zz_3D(1:fstep:end,1:fstep:end,minSlice:maxSlice),...
-                                     Hela_cell(1:fstep:end,1:fstep:end,minSlice:maxSlice),0.7);
-                    
-%%        % Finally, let's display the surface, allocate random colours
-figure
-        h121=subplot(121);
-        h1                  = patch(surf_Nuclei);
-        h1.FaceColor        = 'red';
-        h1.EdgeColor        = 'none';
-        h1.FaceAlpha        = 0.7;
-        h2                  = patch(surf_Cell);
-        h2.FaceColor        = 'blue';
-        h2.EdgeColor        = 'none';
-        h2.FaceAlpha        = 0.2;
-            rotate3d on
-    %title(,'interpreter','none')
-
-%
-view(80,-5)
- lighting('phong');
-hLight1 = camlight ('left');
-axis tight
-grid on
-title('(a)','fontsize',18)
-       h122=subplot(122);
-        h4                  = patch(surf_Nuclei);
-        h4.FaceColor        = 'red';
-        h4.EdgeColor        = 'none';
-        h4.FaceAlpha        = 0.7;
-        h5                  = patch(surf_Cell);
-        h5.FaceColor        = 'blue';
-        h5.EdgeColor        = 'none';
-        h5.FaceAlpha        = 0.2;
-       title('(b)','fontsize',18) 
-view(200,28)
- lighting('phong');
-hLight2 = camlight ('left');
-%hLight2 = camlight ('right');
-axis tight
-grid on
-%
-set(gcf,'position',[ 500  400  900  300])
-
-%
-
-hLight1.Position =[ 5065.0   -1615   -151]; 
-hLight2.Position =[-12541    16485   -353];
-%
-h1.FaceAlpha        = 1;
-h2.FaceAlpha        = 0.1;
-h4.FaceAlpha        = 1;
-h5.FaceAlpha        = 0.1;
-%%
-h121.Position=[0.05 0.1 0.44 0.8];
-h122.Position=[0.54 0.1 0.44 0.8];
-%%
-
-filename ='Fig3.png';
-print('-dpng','-r300',filename)
+ h121.Position = [0.05 0.08 0.44 0.8];
+ h122.Position = [0.54 0.08 0.44 0.8];
+ 
+ %%
+  print('-dpng','-r400','Fig3.png')
